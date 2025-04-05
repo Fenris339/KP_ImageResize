3a8380e69946da96975dd299c5b20dbacfa8e5c5
Boris <fenris339.main@gmail.com>
2025-04-05 12:09:03 +0300

    base logic

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from PyQt6 import QtCore
-
+from image import CImage
 
 def getImages():
     pass
 
 
 def main():
-    pass
+    testImage = CImage('./res/icon.ico')
+    testImage.testPrint()
 
 
 if __name__ == '__main__':
-    pass
+    main()
