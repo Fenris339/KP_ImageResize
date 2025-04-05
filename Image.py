201b2b911dda7f2c9d28ec0b59af95fa4c2a3018
Boris <fenris339@gmail.com>
2025-04-05 19:45:15 +0300

    добавил имя файла

--- Image.py
+++ Image.py
@@ -10,12 +10,13 @@ class CImage:
 
         if self.path and os.path.exists(self.path):
             image = Image.open(path)
-            self.sourceImageMatrix = np.array(image)
+            self._fileName = self.path.split('/')[-1]
+            self._sourceImageMatrix = np.array(image)
         else:
-            self.sourceImageMatrix = None
+            self._sourceImageMatrix = None
 
-        if self.sourceImageMatrix is not None:
-            self.sourceHeight, self.sourceWidth, self.channels = self.sourceImageMatrix.shape
+        if self._sourceImageMatrix is not None:
+            self._sourceHeight, self._sourceWidth, self._channels = self._sourceImageMatrix.shape
 
         self.destinationHeight = 0
         self.destinationWidth = 0
@@ -23,11 +24,36 @@ class CImage:
         self.imageIdent = True
 
 
+    def getFileName(self):
+        return self._fileName
+
+
     def setNeededSize(self, width, height):
-        if (self.sourceWidth != width or self.sourceHeight != height) and (self.sourceWidth and self.sourceHeight):
+        if (self._sourceWidth != width or self._sourceHeight != height) and (self._sourceWidth and self._sourceHeight):
             self.destinationWidth = width
             self.destinationHeight = height
             self.imageIdent = False
+
+
+    def resize(self):
+        if self.imageIdent:
+            return
+
+        scaleX = (self._sourceWidth - 1) / (self.destinationWidth - 1)
+        scaleY = (self._sourceHeight - 1) / (self.destinationHeight - 1)
+
+        self.destinationImageMatrix = np.empty((self.destinationHeight, self.destinationWidth, self._channels),
+                                               dtype=self._sourceImageMatrix.dtype)
+
+        for destinationY in range(self.destinationHeight):
+            for destinationX in range(self.destinationWidth):
+                sourceX = destinationX * scaleX
+                sourceY = destinationY * scaleY
+
+                sourceX_rounded = self.clamp(round(sourceX), 0, self._sourceWidth - 1)
+                sourceY_rounded = self.clamp(round(sourceY), 0, self._sourceHeight - 1)
+
+                self.destinationImageMatrix[destinationY, destinationX] = self._sourceImageMatrix[sourceY_rounded, sourceX_rounded]
 
 
     def saveImage(self):
@@ -37,31 +63,8 @@ class CImage:
         newImage.save(self.path)
 
 
-    def resize(self):
-        if self.imageIdent:
-            return
-
-        scaleX = (self.sourceWidth - 1) / (self.destinationWidth - 1)
-        scaleY = (self.sourceHeight - 1) / (self.destinationHeight - 1)
-
-        self.destinationImageMatrix = np.empty((self.destinationHeight, self.destinationWidth, self.channels),
-                                               dtype=self.sourceImageMatrix.dtype)
-
-        for destinationY in range(self.destinationHeight):
-            for destinationX in range(self.destinationWidth):
-                sourceX = destinationX * scaleX
-                sourceY = destinationY * scaleY
-
-                sourceX_rounded = self.clamp(round(sourceX), 0, self.sourceWidth - 1)
-                sourceY_rounded = self.clamp(round(sourceY), 0, self.sourceHeight - 1)
-
-                self.destinationImageMatrix[destinationY, destinationX] = self.sourceImageMatrix[sourceY_rounded, sourceX_rounded]
-
-
     @staticmethod
     def clamp(value, minimum, maximum):
         return max(minimum, min(value, maximum))
 
 
-    def testPrint(self):
-        print(f"Высота: {self.sourceHeight}\nШирина: {self.sourceWidth}")
--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from Image import CImage
 
 def main():
     testImage = CImage('C:/Users/fenri/PycharmProjects/KP_ImageResize/res/321.png')
-    testImage.setNeededSize(300,300)
+    testImage.setNeededSize(250,75)
     testImage.resize()
     testImage.saveImage()
-    testImage.testPrint()
+    print(testImage.getFileName())
 
 
 if __name__ == '__main__':
